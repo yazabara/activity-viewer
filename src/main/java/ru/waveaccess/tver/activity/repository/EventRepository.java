ced13a1c8d783e53b7994ba1144fdcc968871162
Yaroslav Zabara <yaroslav.zabara@waveaccess.ru>
2018-08-20 17:48:49 +0300

    - idea files: removed from git; - event repository added; - application boot added;

---
+++ src/main/java/ru/waveaccess/tver/activity/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package ru.waveaccess.tver.activity.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import ru.waveaccess.tver.activity.model.event.Event;
+
+@Repository
+public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
+}
