8cd4b263da6896c59720c4631e2464e417d32a2a
Yaroslav Zabara <yaroslav.zabara@waveaccess.ru>
2018-08-21 12:16:35 +0300

    - ToString lombok fixed;

--- src/main/java/ru/waveaccess/tver/activity/model/chat/ChatMessage.java
+++ src/main/java/ru/waveaccess/tver/activity/model/chat/ChatMessage.java
@@ -11,7 +11,7 @@ import java.sql.Timestamp;
 
 @Data
 @EqualsAndHashCode
-@ToString
+@ToString(exclude = "chat")
 @NoArgsConstructor
 
 @Entity
--- src/main/java/ru/waveaccess/tver/activity/model/condition/Condition.java
+++ src/main/java/ru/waveaccess/tver/activity/model/condition/Condition.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 
 @Data
 @EqualsAndHashCode
-@ToString
+@ToString(exclude = "event")
 @NoArgsConstructor
 
 @Entity
